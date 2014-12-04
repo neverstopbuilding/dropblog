5c34c61d900f4f2db9b47ca31dc0251c3668d490
Jason Fox <jasonrobertfox@gmail.com>
2014-12-04 17:20:04 -0500

    Update gem file.

--- app/jobs/process_changes_job.rb
+++ app/jobs/process_changes_job.rb
@@ -9,22 +9,6 @@ class ProcessChangesJob < ActiveJob::Base
     access_token = ENV['dropbox_access_token']
 
     client = DropboxClient.new(access_token)
-    # puts "linked account:", client.account_info().inspect
-
-    # cursor = File.read('experimental/cursor.txt')
-
-    # cursor = cursor.empty? ? nil : cursor
-    # TODO: you could put a time since last request checker or something
-
-    # path_prefix = REDIS.get 'path_prefix'
-    # if path_prefix != dropbox_blog_dir
-    #   logger.info "Current prefix of '#{path_prefix}' does not match configured value of '#{dropbox_blog_dir}' updating..."
-    #   path_prefix = dropbox_blog_dir
-    #   REDIS.set 'path_prefix', path_prefix
-    #   cursor = nil
-    # else
-    #   cursor = REDIS.get 'dropbox_delta_cursor'
-    # end
 
     cursor = REDIS.get 'dropbox_delta_cursor'
     delta = client.delta(cursor, "/#{dropbox_blog_dir}")

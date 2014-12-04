c202d761504c5dd1186f94b511dc688f92f9acd4
Jason Fox <jasonrobertfox@gmail.com>
2014-12-04 16:45:00 -0500

    Update prefix handling.

--- app/models/article.rb
+++ app/models/article.rb
@@ -37,8 +37,11 @@ class Article < ActiveRecord::Base
   end
 
   def self.process_raw_file(slug, content)
-    title = /^#\s?(.+)\n/.match(content)[1].titleize
+
+    title_match = /^#\s?(.+)\n/.match(content)
+    return nil unless title_match
     content.gsub!(/^#\s?.+\n/, '').strip!
+    title = title_match[1].titleize
     article = self.find_or_initialize_by(slug: slug)
     article.title = title
     article.content = content

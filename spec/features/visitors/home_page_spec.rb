e899e8fe23766bcde9d9c2fe051e6080f3c5eb9d
Jason Fox <jasonrobertfox@gmail.com>
2014-12-04 21:14:13 -0500

    Add nice footer.

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -4,13 +4,10 @@
 #   So I can learn more about the website
 feature 'Home page' do
 
-  # Scenario: Visit the home page
-  #   Given I am a visitor
-  #   When I visit the home page
-  #   Then I see "Welcome"
-  scenario 'visit the home page' do
+  scenario 'default menu items' do
     visit root_path
-    expect(page).to have_content 'Welcome'
+    expect(page).to have_content 'Projects'
+    expect(page).to have_content 'Articles'
   end
 
 end

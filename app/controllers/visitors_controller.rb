cb00ffa0f55befb77a6882bed2b3d5f8331753de
Jason Fox <jasonrobertfox@gmail.com>
2014-12-17 14:49:40 -0500

    Add recent articles to home page.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,5 @@
 class VisitorsController < ApplicationController
+  def index
+    @articles = Article.recent
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :pictures, as: :imageable
+  has_many :pictures, as: :pictureable
   has_many :articles
 
   def to_param

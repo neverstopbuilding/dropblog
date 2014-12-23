f122b6fe142de69ba75310d50097254c0a0f4dba
Jason Fox <jasonrobertfox@gmail.com>
2014-12-22 21:05:30 -0500

    Update category image loading.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def title_picture_path(document)
     document.title_picture.public_path if document.title_picture
   end
+
+  def string_color(string)
+    r = to_hex_number(string[0])
+    g = to_hex_number(string[1])
+    b = to_hex_number(string[2])
+    "rgb(#{r}, #{g}, #{b}"
+  end
+
+  private
+
+  def to_hex_number(letter)
+    (((letter.downcase.ord - 97)/26.0) * 255).to_i
+  end
 end

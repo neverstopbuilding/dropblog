42984ff2790fd167d853a54e108449fd9d493303
Jason Fox <jasonrobertfox@gmail.com>
2014-12-30 00:08:40 -0500

    link and usability improvements.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,17 +12,17 @@ module ApplicationHelper
 
   def facebook_link(document)
     url = polymorphic_url([:short, document])
-    link_to 'Facebook', "//www.facebook.com/sharer.php?u=#{url}&t=#{document.title}", class: 'social-button'
+    link_to 'Facebook', "//www.facebook.com/sharer.php?u=#{url}&t=#{document.title}", class: 'social-button', title: 'share on facebook'
   end
 
   def twitter_link(document)
     url = polymorphic_url([:short, document])
-    link_to 'Twitter', "//twitter.com/intent/tweet?url=#{url}&text=#{document.title}&via=jasonrobertfox", class: 'xml plain social-button'
+    link_to 'Twitter', "//twitter.com/intent/tweet?url=#{url}&text=#{document.title}&via=jasonrobertfox", class: 'xml plain social-button', title: 'share on twitter'
   end
 
   def google_plus_link(document)
     url = polymorphic_url([:short, document])
-    link_to 'Google Plus', "//plus.google.com/share?url=#{url}", class: 'social-button'
+    link_to 'Google Plus', "//plus.google.com/share?url=#{url}", class: 'social-button', title: 'share on google plus'
   end
 
   private
--- lib/dropblog/redcarpet/insert_image_renderer.rb
+++ lib/dropblog/redcarpet/insert_image_renderer.rb
@@ -18,12 +18,12 @@ module Dropblog
           link = Pathname.new(link).basename.to_s
           picture = @pictures.find_by_file_name(link)
           if picture
-            wrap_in_picture_div("<img src=\"#{picture.public_path}\" alt=\"#{alt_text}\" title=\"#{title}\">")
+            wrap_in_picture_div("<img src=\"#{picture.public_path}\" alt=\"#{alt_text}\" title=\"#{title}\" />")
           else
             ""
           end
         else
-          wrap_in_picture_div("<img src=\"#{link}\" alt=\"#{alt_text}\" title=\"#{title}\">")
+          wrap_in_picture_div("<img src=\"#{link}\" alt=\"#{alt_text}\" title=\"#{title}\" />")
         end
       end
 
